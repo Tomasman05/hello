0edddd540c55f0d0004013fe1aadbf69924bcac6
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-06-08 10:17:07 +0200

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'Kutyáidat sétáltatod?';
+  name = 'Lajos';
+
+  employee={
+    name: 'Erős István',
+    city: 'Szeged',
+    salary: 576
+  }
+
+  employees=[
+    {name:'Brancs Ádám', city:'Szolnok',salary:555},
+    {name:'Ferenc', city:'Budapest',salary:633},
+    {name:'Zoli', city:'Pécs',salary:587},
+    {name:'Éva', city:'Eger',salary:122},
+    {name:'Evelin', city:'Miskolc',salary:210},
+  ]
+
+  constructor(){
+    this.kiir('henlo henlo sziasztok Nosika itt van')
+  }
+  kiir(msg:string){
+    console.log(msg)
+    console.log(this.employee.name)
+  }
 }
